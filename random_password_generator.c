4056d2eb233ed1cfb74f1030c29a2affd5ec249f
Cameron Anglin <CodeThat@users.noreply.github.com>
2014-01-07 18:22:59 -0600

    Create random_password_generator.c

---
+++ random_password_generator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define minimum_password_length = 8
+#define maximum_password_length = 15
+#define dostuff
+
+
+int main()
+{
+characters = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,w,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,W,Z,1,2,3,4,5,6,7,8,9,0,!,@,#,%,^,&,*,(,),_,-,+,=,|,\,<,>,?,/}
+
+int i;
+char counter = 8; 
+
+	rand();
+		
+while(counter = 8, counter < 16,counter++ ) {
+	printf("rand[%d]+%d",i, rand())
+}
+
+getchar();
+return 0;
+
+}
+
